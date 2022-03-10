fbefef9213167cd101551daf6eab0658148dd5b3
M0ngi <saidanemongi@gmail.com>
2022-03-10 16:30:30 +0100

    Re-organize code & fix logs

--- index.js
+++ index.js
@@ -1,68 +1,26 @@
-const { app } = require("./config");
-const express = require("express");
+require("./config");
+require('./auth');
+
 const firestore = require('./firestore');
-const cookieParser = require('cookie-parser');
 const path = require('path');
 const log = require('./log');
-var requestIp = require('request-ip');
+const requestIp = require('request-ip');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-//username and password
-const myusername = process.env.LOGIN_USER ?? "REDACTED"
-const mypassword = process.env.LOGIN_PASS ?? "REDACTED"
-
-let authToken = "";
-
-app.get("/api/signout", (req, res) =>{
-    req.session.destroy();
-    res.json({code: 200, res: "Logged out"})
-})
-
-app.post("/api/authAdmin", (req, res) =>{
-    if(req.session.userid){
-        var clientIp = requestIp.getClientIp(req);
-        log.logEvent(clientIp, "Signin", level=1);
-
-        res.json({ code: 200 })
-        return;
-    }
-
-    if(req.body.uname !== myusername || req.body.pass != mypassword){
-        var clientIp = requestIp.getClientIp(req);
-        log.logEvent(clientIp, "Signin", comment="Bad creds: " + req.body.uname + ":" + req.body.pass, level=2);
-
-        res.json({ code: 498, error: "Bad request." })
-        return;
-    }
-
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    authToken = Array(125).fill(0).map(x => Math.random().toString(36).charAt(2)).join('');
-
-    req.session.userid = myusername;
-    req.session.token = authToken;
-
-    res.json({ code: 200 });
-    
-    var clientIp = requestIp.getClientIp(req);
-    log.logEvent(clientIp, "Signin", level=1);
-});
+const app = global.app;
 
 app.get("/api/getUsers", async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true')
 
     if(!req.session.userid || !req.session || !req.session.token){
         var clientIp = requestIp.getClientIp(req);
-        log.logEvent(clientIp, "Get users", level=2, comment="No session");
+        log.logEvent(clientIp, "Get users", 2, "No session");
 
         res.json({code: 499, error: "Not authed"});
         return;
     }
-    if(req.session.token !== authToken){
+    if(req.session.token !== global.authToken){
         var clientIp = requestIp.getClientIp(req);
-        log.logEvent(clientIp, "Get users", level=2, comment="Invalid token");
+        log.logEvent(clientIp, "Get users", 2, "Invalid token");
 
         res.json({code: 498, error: "Not authed"});
         return;
@@ -78,22 +36,46 @@ app.get("/api/getUsers", async (req, res) => {
 
     } catch(e){
         console.log(e);
-        log.logEvent(clientIp, "Get users", comment=e, level=2);
+        log.logEvent(clientIp, "Get users", 2, e);
         res.json({ code: 500, error: e});
     }
+});
+
+app.get("/api/getLogs", async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+    if(!req.session.userid || !req.session || !req.session.token){
+        var clientIp = requestIp.getClientIp(req);
+        log.logEvent(clientIp, "View logs", 2, "No session");
+
+        res.json({code: 499, error: "Not authed"});
+        return;
+    }
+    if(req.session.token !== global.authToken){
+        var clientIp = requestIp.getClientIp(req);
+        log.logEvent(clientIp, "View logs", 2, "Invalid token");
+
+        res.json({code: 498, error: "Not authed"});
+        return;
+    }
+
+    res.json({code: 200, res: log.getLogs()});
+
+    var clientIp = requestIp.getClientIp(req);
+    log.logEvent(clientIp, "View logs", 0);
 });
 
 app.post("/api/changeUserStatus", async (req, res)=>{
     if(!req.session.userid || !req.session || !req.session.token){
         var clientIp = requestIp.getClientIp(req);
-        log.logEvent(clientIp, "Change user status", level=2, comment="No session");
+        log.logEvent(clientIp, "Change user status", 2, "No session");
 
         res.json({code: 499, error: "Not authed"});
         return;
     }
-    if(req.session.token !== authToken){
+    if(req.session.token !== global.authToken){
         var clientIp = requestIp.getClientIp(req);
-        log.logEvent(clientIp, "Change user status", level=2, comment="Invalid token");
+        log.logEvent(clientIp, "Change user status", 2, "Invalid token");
 
         res.json({code: 498, error: "Not authed"});
         return;
@@ -102,7 +84,7 @@ app.post("/api/changeUserStatus", async (req, res)=>{
     // curl "http://localhost:3001/api/changeUserStatus" -d "{\"test\": true}" -H "Content-Type: application/json"
     if(!req.body.uid || req.body.paid === undefined){
         var clientIp = requestIp.getClientIp(req);
-        log.logEvent(clientIp, "Change user status", level=2, comment="Invalid parameters");
+        log.logEvent(clientIp, "Change user status", 2, "Invalid parameters");
 
         return res.json({ code: 501, error: "Missing parameters"});
     }
@@ -110,7 +92,7 @@ app.post("/api/changeUserStatus", async (req, res)=>{
     res.json( await firestore.updateUserPayment(req.body.uid, req.body.paid) );
     
     var clientIp = requestIp.getClientIp(req);
-    log.logEvent(clientIp, "Change user status", level=2, comment="Change " + req.body.uid + " to " + (req.body.paid ? "paid" : "not paid"));
+    log.logEvent(clientIp, "Change user status", 2, "Change " + req.body.uid + " to " + (req.body.paid ? "paid" : "not paid"));
 })
 
 // The "catchall" handler: for any request that doesn't
--- log/index.js
+++ log/index.js
@@ -12,7 +12,12 @@ const LEVELS = ["NORMAL", "HIGH", "CRITICAL"]
 module.exports.logEvent = (ip, event, level=0, comment="")=>{
     fs.appendFile(
         LOG_NAME, 
-        "[" + LEVELS[level] + "]" + "[" + getCurrentDate() + "]" + "[" + ip + "] " + event + " - " + comment + "\n",
+        "[" + LEVELS[level] + "]" + "[" + getCurrentDate() + "]" + "[" + ip + "] " + event + (comment === "" ? "" : " - " + comment) + "\n",
         (err)=>{console.log(err);}
         );
+}
+
+module.exports.getLogs = ()=>{
+    let data = fs.readFileSync(LOG_NAME, "utf-8").trim();
+    return data.split("\n");
 }
